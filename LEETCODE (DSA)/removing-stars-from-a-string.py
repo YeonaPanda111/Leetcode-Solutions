508ee3ee47c82d702853c374a3dea46bfbe0dc31
YeonaPanda111 <yashasviprajapati4@gmail.com>
2025-12-09 20:41:28 +0530

    Update removing-stars-from-a-string.py

---
+++ LEETCODE (DSA)/removing-stars-from-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        l=[]
+        for i in s:
+            if i!="*":
+                l.append(i)
+            else:
+                l.pop()
+        return "".join(l)
